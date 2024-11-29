e74004676bb845aba84d9041f928a0b8951dad84
unknown <nataligenius1@gmail.com>
2024-11-29 20:03:32 +0200

    коммит

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ async function startServer() {
     console.log('Connected to MongoDB');
 
     // Запуск Koa-сервера
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
+    app.listen(port, '0.0.0.0', () => {
+      console.log(`Server running at http://0.0.0.0:${port}`);
     });
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
