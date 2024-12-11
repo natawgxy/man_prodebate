8a9c50b6e78723690d01fd234cf57e0146dec0ef
unknown <nataligenius1@gmail.com>
2024-12-11 13:00:23 +0200

    Добавила генерацию роли и темы

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const timer = document.querySelector('.timer')
-    let time_left = 15 * 60
+    let time_left = 17 * 60
     function format_time(seconds){
         const minutes = Math.floor(seconds / 60)
         const sec = seconds % 60
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return `${minstr}:${secstr}`
     }
     function start_timer(){
+        // setInterval - функция выполняется через указанные миллисекунды
         const time_interval = setInterval(() => {
             time_left--
             timer.textContent = format_time(time_left)
@@ -20,5 +21,32 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }, 1000)
     }
+    function role_generation(){
+        const map = new Map()
+        map.set(0, "опозицію (виступаєте 'проти' теми)")
+        map.set(1, "уряд (виступаєте 'за' тему)")
+        let ind = Math.floor(Math.random() * 2)
+        return map.get(ind)
+    }
+    function topic_generation(){
+        const topics = [
+            "Інфослайд: Пᴇᴩᴄᴏнᴀж хʙᴏᴩіє нᴀ ᴩᴀᴋ. Ліᴋᴀᴩ ᴋᴀжᴇ йᴏʍу, щᴏ йᴏʍу ɜᴀᴧиɯиᴧᴏᴄь жиᴛи щᴇ ᴏдин ᴩіᴋ. У ᴨᴇᴩᴄᴏнᴀжᴀ є ʙибіᴩ: ᴨᴩᴏʙᴇᴄᴛи ᴏᴄᴛᴀнні чᴀᴄи ᴄʙᴏᴦᴏ жиᴛᴛя ɜ ᴄіʍ'єю чи ʙиᴩуɯиᴛи у нᴀʙᴋᴏᴧᴏᴄʙіᴛню ᴨᴏдᴏᴩᴏж. Резолюція:Ця Пᴀᴧᴀᴛᴀ ʙʙᴀжᴀє, щᴏ ᴨᴏᴛᴩібнᴏ ɜᴀᴧиɯиᴛиᴄь вдᴏʍа",
+            "Резолюція: Ця Пᴀᴧᴀᴛᴀ ʙʙᴀжᴀє, щᴏ ᴨᴏᴛᴩібнᴏ Діду Мᴏᴩᴏɜу (Сʙяᴛᴏʍу Миᴋᴏᴧᴀю) 'ᴩᴏɜбиᴩᴀᴛиᴄя' ɜ діᴛьʍи ɜі ᴄᴨиᴄᴋу 'ᴨᴏᴦᴀнціʙ'",
+            "Інфослайд: Ви диᴛинᴀ - 10 ᴩᴏчᴋіʙ. Жиʙᴇᴛᴇ у ᴄучᴀᴄнᴏʍу Хᴀᴩᴋᴏʙі. Вᴀɯі бᴀᴛьᴋи ᴨᴩᴏᴨᴏнуюᴛь Вᴀʍ ʙиїхᴀᴛи ɜᴀ ᴋᴏᴩдᴏн. Нᴀ яᴋий ᴛᴇᴩʍін нᴇ ʙідᴏʍᴏ. Резолюція: ЦП ʙʙᴀжᴀє, щᴏ ᴨᴏᴛᴩібнᴏ ᴨᴏᴦᴏдиᴛиᴄя нᴀ ᴨᴩᴏᴨᴏɜицію.",
+            "Резолюція: Ця Палата вважає, що бальне оцінювання не сприяє розвитку учнів та не є ефективним в освітньому процесі"
+        ]
+        let ind = Math.floor(Math.random() * topics.length)
+        return topics[ind]
+    }
+    function set_role(){
+        const role = document.querySelector('.role')
+        role.textContent = role_generation()
+    }
+    function set_topic(){
+        const topic = document.querySelector('.topic')
+        topic.textContent = topic_generation()
+    }
+    set_role()
+    set_topic()
     start_timer()
 })
